edd271a7a9249be6728cfdfa1a68226cc3516943
houN <houqn@zparkhr.com.cn>
2020-09-30 11:00:28 +0800

    this is the commit with pycharm

--- testapp/views.py
+++ testapp/views.py
@@ -10,3 +10,8 @@ def hello(request):
 def dev_test(request):
     print('这是dev分支上的code')
     return HttpResponse('ok')
+
+# 继续设置视图函数 使用pycharm自身所带的工具 替换git命令
+def index(request):
+    print('使用pycharm工具完成')
+    return HttpResponse('index')
