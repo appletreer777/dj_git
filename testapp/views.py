ebad6b2a8a9242b69cfdf625dd3607e3e9af41f3
houN <houqn@zparkhr.com.cn>
2020-09-30 10:42:12 +0800

    this is the commit on the dev for the uncompleted view

--- testapp/views.py
+++ testapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def hello(request):
     return HttpResponse('hello world')
+
+
+# 在dev分支上书写代码
+def dev_test(request):
+    print('这是dev分支上的code')
+    return Http
